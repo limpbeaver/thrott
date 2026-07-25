83212a71385f3b69336ed812b9ee7d4832b1bc87
gyndar <dreaopuckett@gmail.com>
2026-07-25 11:54:31 -0700

    Create engine.lisp

---
+++ engine.lisp
@@ -0,0 +1,14 @@
+(defparameter engine
+    '(0 0 0 0))
+
+(defun cycle (engine)
+    (incf (second engine)))
+
+(defun ignition (engine)
+    ;; establish heat/strain PDEs
+    ;; linkage constraint sim
+    ;; actuator linkage position
+    )
+
+(cycle engine)
+(print engine)
